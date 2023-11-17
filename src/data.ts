da5eac8a80b99808df5b13f51b5b97cb8fa256d2
Diogo maçal <119339116+D1ogooo@users.noreply.github.com>
2023-11-17 12:41:33 -0300

    Atualizar o src

---
+++ src/data.ts
@@ -0,0 +1,27 @@
+interface typstar {
+ _id: number,
+ mark: boolean,
+}
+
+export const StarsInfo:typstar[] = [
+ {
+  _id:0,
+  mark: false,
+ },
+ {
+  _id:1,
+  mark: false,
+ },
+ {
+  _id:2,
+  mark: false,
+ },
+ {
+  _id:3,
+  mark: false,
+ }
+ {
+  _id:4,
+  mark: false,
+ }
+]
